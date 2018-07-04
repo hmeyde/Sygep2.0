2427f1da19e921fa4c3aa543cc198feb565d635f
hmeyde <ahmedmohameden41@gmail.com>
2018-07-04 17:51:08 +0200

    sygep2.0

--- src/main/java/mr/mbconsulting/Sygep/SygepApplication.java
+++ src/main/java/mr/mbconsulting/Sygep/SygepApplication.java
@@ -8,16 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan({"mr.mbconsulting.Sygep.controller","mr.mbconsulting.Sygep.services"})
+@EntityScan("mr.mbconsulting.Sygep.model")
+@EnableJpaRepositories("mr.mbconsulting.Sygep.repository")
 public class SygepApplication  implements CommandLineRunner{
 
 	@Autowired
 	UserService userService;
-
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SygepApplication.class, args);
 	}
---
+++ src/main/java/mr/mbconsulting/Sygep/repository/ClientRepository.java
@@ -0,0 +1,16 @@
+package mr.mbconsulting.Sygep.repository;
+
+import mr.mbconsulting.Sygep.model.Client;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Integer> {
+	
+}
